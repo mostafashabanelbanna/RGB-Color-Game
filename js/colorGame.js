ff207ad742677d18f6192e6ace345deb8ed30306
Mostafa <dashassas@gmail.com>
2019-02-19 03:54:06 +0200

    handle reset button

--- js/colorGame.js
+++ js/colorGame.js
@@ -1,9 +1,27 @@
 var colors = generatRandomColor(6);
-
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 var message = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function() {
+	//generate all new colors
+	colors = generatRandomColor(6);
+	//pick a new random color for array
+	pickedColor = pickColor();
+	//change colorDisplay to match picked color
+	colorDisplay.textContent = pickedColor;
+	//change colors of squars
+	for(var i = 0; i < squares.length; i++){
+	//add initial colors to squares
+	squares[i].style.backgroundColor = colors[i];
+	}
+	//change h1 background color
+	h1.style.backgroundColor = "#232323";
+	resetButton.textContent = "New colors";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -19,6 +37,8 @@ for(var i = 0; i < squares.length; i++){
 		if (clickedColor === pickedColor){
 			message.textContent = "Correct!";
 			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
+			resetButton.textContent = "Play Again?";
 		}else{
 			this.style.backgroundColor = "#232323";
 			message.textContent = "Try Again";
@@ -54,5 +74,5 @@ function randomColor() {
 	var r = Math.floor(Math.random() * 266);
 	var g = Math.floor(Math.random() * 266);
 	var b = Math.floor(Math.random() * 266);
-	return "rgb(" + r + "," + g + "," + b + ")";
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
